e074daac716bb0fea4ec8471c1ef0e9e7e22f0e9
Bala Chandar <chanbala555@gmail.com>
2022-12-14 12:13:59 +0530

    Commit

---
+++ product/src/App.js
@@ -0,0 +1,29 @@
+//import logo from './logo.svg';
+import './App.css';
+import Display from './component/Display';
+//import GrandParent from './component/GrandParent';
+//import { UserProvider } from './component/UserContext';
+//import Demoform from './component/Demoform';
+
+//import Parent from './component/Parent';
+//import Ternary from './component/ternary';
+//import Listpost from './component/Listpost';
+
+function App() {
+  return (
+    <div className="App">
+       {/* <Parent /> */}
+       {/* <Listpost /> */}
+       {/* <Demoform /> */}
+     {/* <UserProvider value="Bala">
+         <GrandParent /> 
+
+     </UserProvider> */}
+     <Display />
+        
+       
+    </div>
+  );
+}
+
+export default App;
